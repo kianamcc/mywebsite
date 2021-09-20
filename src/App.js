76570b7301a4df0cccad24e1296ece00801f2245
Kiana <kiana@Kianas-MacBook-Pro.local>
2021-09-19 21:53:29 -0700

    Navbar, Sidebar, Mobile Testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css" // using bootstrap
 import React from "react";
+import {BrowserRouter as Router} from 'react-router-dom'
+import Home from './pages'
+// import About from './pages/about';
+// import Contact from './pages/contact';
+// import Projects from './pages/projects';
 
 function App() {
   return (
-    <h2>Kiana McCullough</h2>
+    <Router>
+      {/* <Sidebar />
+      <Navbar /> */}
+      <Home />
+      {/* <Switch>
+          <Route path="/" exact component={Home} />'<Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/projects" exact component={Projects} />
+          <Route path="/contact" exact component={Contact} />
+      </Switch> */}
+    </Router>
   );
 }
 
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink } from './SidebarElements';
+
+const Sidebar = ({isOpen, toggle}) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon />
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="projects">
+                        Projects
+                    </SidebarLink>
+                    <SidebarLink to="contact">
+                        Contact
+                    </SidebarLink>
+                </SidebarMenu>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
